f5f591450d1aa20b6ca93caf32023c1414f4b5a8
Rosa Tu <rosalina.tu@gmail.com>
2018-06-27 13:49:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  sleep 1 
+  number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
